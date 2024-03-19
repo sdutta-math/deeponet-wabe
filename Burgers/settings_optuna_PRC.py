a2cc8a5dde02139e5df2a37eab02cf0a6d3b2abe
priverc <85243635+priverc@users.noreply.github.com>
2024-03-19 07:33:26 -1000

    Optuna

--- Burgers/settings_optuna_PRC.py
+++ Burgers/settings_optuna_PRC.py
@@ -1,7 +1,7 @@
-train_epochs = 100 #100000
-optuna_epochs = 30 #3000
+train_epochs = 1 #100000
+optuna_epochs = 3 #3000
 optuna_trials = 5 #200
-optuna_timeout = 1000000
+optuna_timeout = 1000000000
 study_name = 'DON_optim'
 
 ###GENERAL
@@ -31,8 +31,30 @@ neurons_layer_lower = 32
 neurons_layer_upper = 256
 neurons_layer_step = 32
 
-b_number_layers_lower = 1
+neurons_layer_encoder_lower = 32
+neurons_layer_encoder_upper = 128
+neurons_layer_encoder_step = 32
+
+b_number_layers_lower = 2
 b_number_layers_upper = 5
+b_number_layers_step = 1
+
+
+b_number_layers_encoder_lower = 2
+b_number_layers_encoder_upper = 4
+b_number_layers_encoder_step = 1
+
+t_number_layers_encoder_lower = 2
+t_number_layers_encoder_upper = 4
+t_number_layers_encoder_step = 1
+
+l_factor_lower = 2
+l_factor_upper =7
+l_factor_step = 1
+
+l_factor_encoder_lower = 1
+l_factor_encoder_upper = 5
+l_factor_encoder_step = 1
 
 b_actf = ["relu", "elu", "tanh", "swish"]
 
@@ -49,36 +71,55 @@ t_regularizer = ["none", "l1", "l2"]
 
 t_initializer = ["glorot_normal", "glorot_uniform", "he_normal", "he_uniform"]
 
-b_encoder_number_layers_lower = 1
-b_encoder_number_layers_upper = 5
+
 
 b_encoder_actf = ["relu", "elu", "tanh", "swish"]
 
 b_encoder_regularizer = ["none", "l1", "l2"]
 
-b_encoder_initializer = ["glorot_normal", "glorot_uniform", "he_normal", "he_uniform"]
+b_encoder_init = ["glorot_normal", "glorot_uniform", "he_normal", "he_uniform"]
 
-t_encoder_number_layers_lower = 1
-t_encoder_number_layers_upper = 5
 
 t_encoder_actf = ["relu", "elu", "tanh", "swish"]
 
 t_encoder_regularizer = ["none", "l1", "l2"]
 
-t_encoder_initializer = ["glorot_normal", "glorot_uniform", "he_normal", "he_uniform"]
+t_encoder_init = ["glorot_normal", "glorot_uniform", "he_normal", "he_uniform"]
+
+batch_size = [64,128,256,512,1024]
+# batch_size_lower = 32
+# batch_size_upper = 512
+# batch_size_step = 32
+
+init_lr = [1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3]
+
+
+###AUTOENCODER
+ae_steps = 5000 #1000
+ae_factor = 0.9
+ae_learning_rate_decay = True
+
+ae_batch_size = [64,128,256,512,1024]
+
+ae_init_lr = [1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3]
+
+ae_number_layers_lower = 2
+ae_number_layers_upper = 5
+ae_number_layers_step = 1
 
 latent_dim_lower = 12 
 latent_dim_upper = 36
 latent_dim_step = 12
 
-init_lr = [1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3]
 
-batch_size_lower = 32
-batch_size_upper = 512
-batch_size_step = 32
+enc_act = ["relu", "elu", "tanh", "swish"]
+dec_act = ["relu", "elu", "tanh", "swish"]
+
+ae_optimizer = "Adam"
+
 
 ###CALLBACKS
-reduce_patience = 100
+reduce_patience = 5000
 
-early_patience = 100
+# early_patience = 100000000
 
